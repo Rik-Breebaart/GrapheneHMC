6cf7de25de43294cd648e92de11c9bb5f4bea318
Rik-Breebaart <r.s.breebaart@students.uu.nl>
2022-08-24 14:51:23 +0200

    initial commit with some example files

---
+++ app/graphene-no-interactions.jl
@@ -0,0 +1,4 @@
+""" this script will contain the graphene computations without interactions 
+    using the path-integral formalisme used for HMC computations of graphene.
+"""
+
---
+++ src/hamiltonian-no-interactions.jl
@@ -0,0 +1,9 @@
+""" This file contains the functions for the non-interactign system of graphene.
+    We will first provide methods to compare the analytical results, and then 
+    using the path-integral formalism on a discrete time lattice.
+
+"""
+
+
+
+function 
